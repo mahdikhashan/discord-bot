051c7961bbb5fb9c3ee87e74548854878a6d1c4b
Mahdi Khashan <mkhashan@pyango.ch>
2022-05-14 02:09:08 +0430

    revise2

--- functions/server.js
+++
@@ -1,8 +0,0 @@
-import serverless from 'serverless-http'
-import expressApp from './app'
-
-const functionName = "server"
-
-const app = expressApp(functionName)
-
-exports.handler = serverless(app)
