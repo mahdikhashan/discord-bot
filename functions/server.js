e68daeb0a9b04b8befa39f027c9ed26566d3bd1b
Mahdi Khashan <mkhashan@pyango.ch>
2022-05-24 21:30:25 +0430

    add hello world function

--- functions/server.js
+++ functions/server.js
@@ -4,22 +4,16 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-const Discord = require("discord.js")
-
-const client = new Discord.Client()
 
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
-  console.log("asdfasdf")
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+
 router.post('/', (req, res) => {
   res.json({ postBody: req.body })
-  console.log("sdafsdf")
-  console.log(req.body)
 });
 
 app.use(bodyParser.json());
